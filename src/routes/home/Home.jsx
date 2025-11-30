50671f3d4b024837797361cb9ef62a159cda230b
sabs <dziedziochs@gmail.com>
2025-11-30 14:45:21 +0100

    features with animated icons

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -15,6 +15,10 @@ import Sport from "./assets/sport.jpg";
 import Emergency from "./assets/emergency.jpg";
 import Travel from "./assets/travel.jpg";
 
+import Production from "./assets/production.webp";
+import Inventory from "./assets/inventory.webp";
+import Ideas from "./assets/ideas.webp";
+
 const About = () => {
 	return (
 		<section className='contained about'>
@@ -134,12 +138,108 @@ const Services = () => {
 	);
 };
 
+const Mission = () => {
+	return (
+		<div className='mission contained'>
+			<div className='left'>
+				<Heading
+					accent={true}
+					eyebrow='consequatur distinctio dignissimos corrupti'
+					heading='Cumque ratione quis'
+					subheading='consequuntur aspernatur minima'
+				/>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+					nihil deleniti? Quasi inventore non reprehenderit quia sunt, corporis
+					repudiandae voluptatem accusamus ducimus magni atque ipsam asperiores,
+					sit sapiente, vero tempora.
+				</p>
+				<p>
+					Consectetur vitae, aliquid facere aut praesentium natus sequi magni
+					quis quo laudantium excepturi alias veniam? Aperiam, ex sed. Libero,
+					provident.
+				</p>
+			</div>
+			<div className='right'>
+				<img src={Events} alt='' />
+			</div>
+		</div>
+	);
+};
+
+const Features = () => {
+	const features = [
+		{
+			id: 1,
+			title: "Kompleksowa produkcja",
+			subtitle: "od idei po realizację",
+			pointers: [
+				"distinctio dignissimos corrupti",
+				"consequuntur aspernatur minima",
+				"dolor dignissimos aspernatur",
+			],
+			image: Production,
+		},
+		{
+			id: 2,
+			title: "Własne zaplecze",
+			subtitle: "scenograficzne i techniczne",
+			pointers: [
+				"aspernatur minima",
+				"distinctio dignissimos aspernatur",
+				"laudantium excepturi alias veniam",
+			],
+			image: Inventory,
+		},
+		{
+			id: 3,
+			title: "Autorskie pomysły",
+			subtitle: "gry i formaty immersyjne",
+			pointers: [
+				"distinctio dignissimos aspernatur",
+				"voluptatem accusamus",
+				"aperiam, ex sed",
+			],
+			image: Ideas,
+		},
+	];
+	return (
+		<div className='features contained'>
+			<Heading
+				centered={true}
+				eyebrow='consequatur distinctio dignissimos corrupti'
+				heading='Lorem lorem'
+				subheading='consequuntur aspernatur minima'
+			/>
+			<div className='features-container'>
+				{features.map((item, id) => (
+					<div className='feature' id={id}>
+						<img src={item.image} alt='' />
+						<div className='title'>
+							<h4>{item.title}</h4>
+							<p className='caption'>{item.subtitle}</p>
+						</div>
+						{/* <p className='caption'>to oznacza:</p> */}
+						<ul>
+							{item.pointers.map((pointer, id) => (
+								<li id={id}>{pointer}</li>
+							))}
+						</ul>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
 const Home = () => {
 	return (
 		<div className='home'>
 			<Hero />
 			<About />
 			<Services />
+			<Mission />
+			<Features />
 			<Logos />
 		</div>
 	);
