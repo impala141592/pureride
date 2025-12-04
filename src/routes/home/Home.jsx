294094c09822ad2bda0fbdb27812a3fe8c702c01
sabs <dziedziochs@gmail.com>
2025-12-04 14:15:39 +0100

    testimonials and small tweaks. responsive

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css"; // basic styles
 import "swiper/css/scrollbar";
 import "swiper/css/navigation";
-import { Navigation, Scrollbar } from "swiper/modules";
+import { Navigation, Scrollbar, Pagination, Autoplay } from "swiper/modules";
 import Logos from "../../components/logos/Logos";
 
 import Wave from "../../assets/wave.svg";
@@ -18,6 +18,10 @@ import Travel from "./assets/travel.jpg";
 import Production from "./assets/production.webp";
 import Inventory from "./assets/inventory.webp";
 import Ideas from "./assets/ideas.webp";
+
+import Avatar from "../../assets/icons/avatar.webp";
+import Janet from "./assets/janet.jpg";
+import Gary from "./assets/gary.jpg";
 
 const About = () => {
 	return (
@@ -138,6 +142,77 @@ const Services = () => {
 	);
 };
 
+const Testimonials = () => {
+	const testimonials = [
+		{
+			avatar: Janet,
+			author: "Janet Jackson",
+			company: "Jackson's 5",
+			quote:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nihil deleniti? Quasi inventore non reprehenderit quia sunt, corporis repudiandae voluptatem ",
+		},
+		{
+			avatar: Gary,
+			author: "Gary Oldman",
+			company: "Milka",
+			quote:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nihil deleniti? Quasi inventore non reprehenderit quia sunt, corporis repudiandae voluptatem ",
+		},
+		{
+			avatar: null,
+			author: "Charles Darwin",
+			company: "Galapagos inc.",
+			quote:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nihil deleniti? Quasi inventore non reprehenderit quia sunt, corporis repudiandae voluptatem. Ex, saepe! Magnam, doloremque assumenda. ",
+		},
+		{
+			avatar: null,
+			author: "Felix Staples",
+			company: "Anonymous",
+			quote:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nihil deleniti?",
+		},
+	];
+	return (
+		<div className='testimonials'>
+			<Swiper
+				className='testimonials-swiper'
+				modules={[Navigation, Pagination, Autoplay]}
+				autoplay={{ delay: 8000, disableOnInteraction: false }}
+				navigation
+				spaceBetween={24}
+				pagination={{ clickable: true }}
+				loop={true}
+			>
+				{testimonials.map((item, index) => (
+					<SwiperSlide className='ttt' key={index}>
+						<div className='testimonial'>
+							<div className='avatar'>
+								{item.avatar ? (
+									<img className='pic' src={item.avatar} alt={item.author} />
+								) : (
+									<img
+										className='placeholder'
+										src={Avatar}
+										alt='placeholder avatar'
+									/>
+								)}
+							</div>
+							<div className='content'>
+								<div className='person'>
+									<span className='author'>{item.author}</span>
+									<span className='company'>{item.company}</span>
+								</div>
+								<p className='quote'>“{item.quote}”</p>
+							</div>
+						</div>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
+};
+
 const Mission = () => {
 	return (
 		<div className='mission contained'>
@@ -161,7 +236,8 @@ const Mission = () => {
 				</p>
 			</div>
 			<div className='right'>
-				<img src={Events} alt='' />
+				{/* <img src={Events} alt='' /> */}
+				<Testimonials />
 			</div>
 		</div>
 	);
@@ -213,7 +289,7 @@ const Features = () => {
 			/>
 			<div className='features-container'>
 				{features.map((item, id) => (
-					<div className='feature' id={id}>
+					<div className='feature' key={id}>
 						<img src={item.image} alt='' />
 						<div className='title'>
 							<h4>{item.title}</h4>
@@ -222,7 +298,7 @@ const Features = () => {
 						{/* <p className='caption'>to oznacza:</p> */}
 						<ul>
 							{item.pointers.map((pointer, id) => (
-								<li id={id}>{pointer}</li>
+								<li key={id}>{pointer}</li>
 							))}
 						</ul>
 					</div>
