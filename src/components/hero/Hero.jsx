66080d8f314a633f490674c264ea7d1d7ab8d2f2
sabs <dziedziochs@gmail.com>
2025-12-06 10:26:46 +0100

    Vamp the top bar. Add mobile menu. WIP: desktop menu after scroll

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,54 +4,9 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import WaveCut from "../../assets/wave-cut.svg";
+import HeroImage from "./bg.png";
 
 import "./Hero.scss";
-
-const HeroS = () => {
-	const slides = [
-		{
-			id: 1,
-			title: "Slide 1",
-			image:
-				"https://images.unsplash.com/photo-1763037152119-f86924827076?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-		},
-		{
-			id: 2,
-			title: "Slide 2",
-			image:
-				"https://images.unsplash.com/photo-1763046289892-857b83dc070f?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyOHx8fGVufDB8fHx8fA%3D%3D",
-		},
-		{
-			id: 3,
-			title: "Slide 3",
-			image:
-				"https://images.unsplash.com/photo-1762715461119-cbd8d82a0aee?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D",
-		},
-	];
-
-	return (
-		<Swiper
-			modules={[Navigation, Pagination, Autoplay]}
-			navigation
-			pagination={{ clickable: true }}
-			autoplay={{ delay: 5000 }}
-			loop={true}
-		>
-			{slides.map((slide) => (
-				<SwiperSlide key={slide.id}>
-					<div
-						className='hero-slide'
-						style={{ backgroundImage: `url(${slide.image})` }}
-					>
-						{/* <div className='slide-copy'>
-							<h1>{slide.title}</h1>
-						</div> */}
-					</div>
-				</SwiperSlide>
-			))}
-		</Swiper>
-	);
-};
 
 const Hero = () => {
 	return (
@@ -61,8 +16,10 @@ const Hero = () => {
 			</div> */}
 			{/* <img src={WaveCut} alt='wave-cut' className='wave-cut' /> */}
 			{/* <div className='hero-ewa'></div> */}
-			<img src='./ewa.png' alt='ewa' className='hero-ewa' />
-			<div className='hero-bg'></div>
+			{/* <img src='./ewa.png' alt='ewa' className='hero-ewa' /> */}
+			<div className='hero-bg'>
+				<img src={HeroImage} alt='' />
+			</div>
 		</div>
 	);
 };
--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -1,5 +1,9 @@
 import "./TopBar.scss";
 import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
+import Wave from "../../assets/wave.svg";
+
+import { useState } from "react";
 
 const navigationItems = [
 	{ text: "historia", to: "/historia" },
@@ -9,15 +13,36 @@ const navigationItems = [
 ];
 
 const Navigation = ({}) => {
+	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<nav>
-			<ul className='navigation'>
+			<ul className='desktop-nav'>
 				{navigationItems.map((item, index) => (
 					<li key={index}>
 						<Link to={item.to}>{item.text}</Link>
 					</li>
 				))}
 			</ul>
+			<div className='mobile-nav'>
+				<button
+					className={isOpen ? "burger open" : "burger"}
+					onClick={() => setIsOpen(!isOpen)}
+				>
+					<div className='bar'></div>
+					<div className='bar'></div>
+					<div className='bar'></div>
+				</button>
+				<ul className={isOpen ? "open" : ""}>
+					{navigationItems.map((item, index) => (
+						<li key={index}>
+							<Link onClick={() => setIsOpen(false)} to={item.to}>
+								{item.text}
+							</Link>
+							<img className='wave' src={Wave} alt='' />
+						</li>
+					))}
+				</ul>
+			</div>
 		</nav>
 	);
 };
@@ -27,8 +52,7 @@ const TopBar = ({}) => {
 		<div className='top-bar-container'>
 			<Link to='/'>
 				<div className='logo'>
-					<span className='pure'>pure</span>
-					<span>ride</span>
+					<img src={Logo} alt='pure ride logo' />
 				</div>
 			</Link>
 			<Navigation />
